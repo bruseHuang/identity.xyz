5e6b8d779661baf1053b79526be6e7c418a8f05b
bruseHuang <124752187+bruseHuang@users.noreply.github.com>
2023-02-13 22:36:23 +0800

    feat: update logo and nav

--- src/app/shared/component/nav-left/nav-left.component.ts
+++ src/app/shared/component/nav-left/nav-left.component.ts
@@ -6,28 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav-left.component.scss'],
 })
 export class NavLeftComponent {
-  navs = [
-    {
-      link: '/',
-      name: 'Home',
-      icon: 'other_houses',
-    },
-    {
-      link: '/',
-      name: 'Get stared',
-      icon: 'apps',
-    },
-    {
-      link: '/',
-      name: 'Develop',
-      icon: 'code',
-    },
-    {
-      link: '/',
-      name: 'Team',
-      icon: 'group_work',
-    },
-  ];
   navHoverIndex = -1;
+  navIndex = 0;
   constructor() {}
 }
