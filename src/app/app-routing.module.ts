c75726a8cd64ceac6ab2d3d6ee6b0c3709f25cda
bruseHuang <124752187+bruseHuang@users.noreply.github.com>
2023-02-22 22:27:24 +0800

    feat: add home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages';
 
-const routes: Routes = [{ path: '**', redirectTo: '/' }];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: '**', redirectTo: '/' },
+];
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
+import { HomeComponent } from './pages';
+
+const PAGECOMPONENTS = [HomeComponent];
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ...PAGECOMPONENTS],
   imports: [
     BrowserModule,
     AppRoutingModule,
